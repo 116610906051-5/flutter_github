c237f139f4b0459a1bc737c1709cbf47044126af
116610906051-5 <156157127+116610906051-5@users.noreply.github.com>
2025-10-16 11:41:18 +0700

    Change displayed text to 'โคเดของ ณพฤทธิ์'

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('5555'),
+          child: Text('โคเดของ ณพฤทธิ์'),
         ),
       ),
     );
